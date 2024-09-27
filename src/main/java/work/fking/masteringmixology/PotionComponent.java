0200d063333b9c82b2d8c7651473325ea4c8d02c
Frank Hansen <frankhvidbjerg@gmail.com>
2024-09-27 17:03:59 +0200

    Move experience to component
    Instead of setting the exp in the PotionType, we can set it in the
    components and calculate the experience in the type based on the
    components.
    This means if exp rates are changed later we can more easily update it.
    
    Also changes the experience numbers to be 10x less, not sure why they were so high

--- src/main/java/work/fking/masteringmixology/PotionComponent.java
+++ src/main/java/work/fking/masteringmixology/PotionComponent.java
@@ -1,16 +1,18 @@
 package work.fking.masteringmixology;
 
 public enum PotionComponent {
-    AGA('A', "00e676"),
-    LYE('L', "e91e63"),
-    MOX('M', "03a9f4");
+    AGA('A', "00e676", 85),
+    LYE('L', "e91e63", 45),
+    MOX('M', "03a9f4", 125);
 
     private final char character;
     private final String color;
+    private final int experience;
 
-    PotionComponent(char character, String color) {
+    PotionComponent(char character, String color, int experience) {
         this.character = character;
         this.color = color;
+        this.experience = experience;
     }
 
     public char character() {
@@ -19,5 +21,9 @@ public enum PotionComponent {
 
     public String color() {
         return color;
+    }
+
+    public int experience() {
+        return experience;
     }
 }
