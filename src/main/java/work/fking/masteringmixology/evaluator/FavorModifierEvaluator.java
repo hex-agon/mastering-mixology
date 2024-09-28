75decc5bea9054513ae7dbbb8d85e36b3b6e61ae
Hexagon <hexagon@fking.work>
2024-09-28 09:22:08 -0300

    add favor retort strategy

--- src/main/java/work/fking/masteringmixology/evaluator/FavorModifierEvaluator.java
+++ src/main/java/work/fking/masteringmixology/evaluator/FavorModifierEvaluator.java
@@ -3,12 +3,18 @@ package work.fking.masteringmixology.evaluator;
 import work.fking.masteringmixology.PotionModifier;
 import work.fking.masteringmixology.PotionOrder;
 
-public class FavorAlembicEvaluator implements PotionOrderEvaluator {
+public class FavorModifierEvaluator implements PotionOrderEvaluator {
+
+    private final PotionModifier modifier;
+
+    public FavorModifierEvaluator(PotionModifier modifier) {
+        this.modifier = modifier;
+    }
 
     @Override
     public PotionOrder evaluate(EvaluatorContext context) {
         for (var order : context.orders()) {
-            if (order.potionModifier() == PotionModifier.CRYSTALISED) {
+            if (order.potionModifier() == modifier) {
                 return order;
             }
         }
