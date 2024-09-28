f3375cd5721e9dfc62354570b59249d6ff78d285
Hexagon <hexagon@fking.work>
2024-09-28 09:43:44 -0300

    add favor retort strategy (#20)

--- src/main/java/work/fking/masteringmixology/Strategy.java
+++ src/main/java/work/fking/masteringmixology/Strategy.java
@@ -1,13 +1,14 @@
 package work.fking.masteringmixology;
 
 import work.fking.masteringmixology.evaluator.BestExperienceEvaluator;
-import work.fking.masteringmixology.evaluator.FavorAlembicEvaluator;
+import work.fking.masteringmixology.evaluator.FavorModifierEvaluator;
 import work.fking.masteringmixology.evaluator.FavorComponentEvaluator;
 import work.fking.masteringmixology.evaluator.PotionOrderEvaluator;
 
 public enum Strategy {
     FAVOR_EXPERIENCE("Favor experience", new BestExperienceEvaluator()),
-    FAVOR_ALEMBIC("Favor alembic", new FavorAlembicEvaluator()),
+    FAVOR_ALEMBIC("Favor alembic", new FavorModifierEvaluator(PotionModifier.CRYSTALISED)),
+    FAVOR_RETORT("Favor retort", new FavorModifierEvaluator(PotionModifier.CONCENTRATED)),
     FAVOR_MOX("Favor mox", new FavorComponentEvaluator(PotionComponent.MOX)),
     FAVOR_AGA("Favor aga", new FavorComponentEvaluator(PotionComponent.AGA)),
     FAVOR_LYE("Favor lye", new FavorComponentEvaluator(PotionComponent.LYE));
