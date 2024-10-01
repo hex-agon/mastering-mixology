1503f66c6ee9ef060cd74093ea525b3e87138098
JDrinkH20 <johndrinkwater@Johns-MacBook-Pro.local>
2024-10-01 10:06:10 -0600

    Added Inventory Overlay

---
+++ src/main/java/work/fking/masteringmixology/MasteringMixologyInventoryOverlay.java
@@ -0,0 +1,38 @@
+package work.fking.masteringmixology;
+
+import javax.inject.Inject;
+
+import net.runelite.api.widgets.WidgetItem;
+import net.runelite.client.game.ItemManager;
+import net.runelite.client.ui.overlay.WidgetItemOverlay;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+class MasteringMixologyInventoryOverlay extends WidgetItemOverlay
+{
+    private final ItemManager itemManager;
+    private final MasteringMixologyPlugin plugin;
+
+    @Inject
+    private MasteringMixologyInventoryOverlay(ItemManager itemManager, MasteringMixologyPlugin plugin, MasteringMixologyConfig config)
+    {
+        this.itemManager = itemManager;
+        this.plugin = plugin;
+        showOnInventory();
+    }
+
+    @Override
+    public void renderItemOverlay(Graphics2D graphics, int itemId, WidgetItem widgetItem)
+    {
+        final Color color = plugin.highlightedItems().get(itemId);
+        if (color == null)
+        {
+            return;
+        }
+
+        Rectangle bounds = widgetItem.getCanvasBounds();
+        final BufferedImage outline = itemManager.getItemOutline(itemId, widgetItem.getQuantity(), color);
+        graphics.drawImage(outline, (int) bounds.getX(), (int) bounds.getY(), null);
+    }
+}
