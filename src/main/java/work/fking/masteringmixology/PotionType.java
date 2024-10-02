17bb28461fd2943b5bc52a6fe3701a7e759e6cfd
Hexagon <hexagon@fking.work>
2024-10-02 19:02:43 -0300

    remove level requiriment checks

--- src/main/java/work/fking/masteringmixology/PotionType.java
+++ src/main/java/work/fking/masteringmixology/PotionType.java
@@ -7,27 +7,25 @@ import static work.fking.masteringmixology.PotionComponent.LYE;
 import static work.fking.masteringmixology.PotionComponent.MOX;
 
 public enum PotionType {
-    MAMMOTH_MIGHT_MIX(60, 1900, MOX, MOX, MOX),
-    MYSTIC_MANA_AMALGAM(63, 2150, MOX, MOX, AGA),
-    MARLEYS_MOONLIGHT(66, 2400, MOX, MOX, LYE),
-    ALCO_AUGMENTATOR(60, 1900, AGA, AGA, AGA),
-    AZURE_AURA_MIX(69, 2650, AGA, AGA, MOX),
-    AQUALUX_AMALGAM(72, 2900, AGA, LYE, AGA),
-    LIPLACK_LIQUOR(60, 1900, LYE, LYE, LYE),
-    MEGALITE_LIQUID(75, 3150, MOX, LYE, LYE),
-    ANTI_LEECH_LOTION(78, 3400, AGA, LYE, LYE),
-    MIXALOT(71, 3650, MOX, AGA, LYE);
+    MAMMOTH_MIGHT_MIX(1900, MOX, MOX, MOX),
+    MYSTIC_MANA_AMALGAM(2150, MOX, MOX, AGA),
+    MARLEYS_MOONLIGHT(2400, MOX, MOX, LYE),
+    ALCO_AUGMENTATOR(1900, AGA, AGA, AGA),
+    AZURE_AURA_MIX(2650, AGA, AGA, MOX),
+    AQUALUX_AMALGAM(2900, AGA, LYE, AGA),
+    LIPLACK_LIQUOR(1900, LYE, LYE, LYE),
+    MEGALITE_LIQUID(3150, MOX, LYE, LYE),
+    ANTI_LEECH_LOTION(3400, AGA, LYE, LYE),
+    MIXALOT(3650, MOX, AGA, LYE);
 
     private static final PotionType[] TYPES = PotionType.values();
 
     private final String recipe;
-    private final int levelReq;
     private final int experience;
     private final PotionComponent[] components;
 
-    PotionType(int levelReq, int experience, PotionComponent... components) {
+    PotionType(int experience, PotionComponent... components) {
         this.recipe = colorizeRecipe(components);
-        this.levelReq = levelReq;
         this.experience = experience;
         this.components = components;
     }
@@ -54,10 +52,6 @@ public enum PotionType {
 
     public String recipe() {
         return recipe;
-    }
-
-    public int levelReq() {
-        return levelReq;
     }
 
     public int experience() {
