e62bc2e49da34962b7343ca7a4790f371a09f176
Frank Hansen <frankhvidbjerg@gmail.com>
2024-10-29 17:43:29 +0100

    Order by name after ordering by modifier
    This way it's a bit cleaner to do the potions in order

--- src/main/java/work/fking/masteringmixology/PotionOrderSorting.java
+++ src/main/java/work/fking/masteringmixology/PotionOrderSorting.java
@@ -5,18 +5,21 @@ import java.util.Comparator;
 public enum PotionOrderSorting {
     VANILLA("Vanilla (random)", null),
     // Sort by modifier, in the order CRYSTALISED > HOMOGENOUS > CONCENTRATED
-    BY_STATION("By station", Comparator.comparingInt(order -> {
-        switch (order.potionModifier()) {
-            case CRYSTALISED:
-                return 0;
-            case HOMOGENOUS:
-                return 1;
-            case CONCENTRATED:
-                return 2;
-            default:
-                return Integer.MAX_VALUE;
-        }
-    }));
+    BY_STATION("By station", Comparator
+            .comparingInt((PotionOrder order) -> {
+                switch (order.potionModifier()) {
+                    case CRYSTALISED:
+                        return 0;
+                    case HOMOGENOUS:
+                        return 1;
+                    case CONCENTRATED:
+                        return 2;
+                    default:
+                        return Integer.MAX_VALUE;
+                }
+            })
+            .thenComparing(order -> order.potionType().name()) // Secondary sorting by PotionType name alphabetically
+    );
 
     private final String name;
     private final Comparator<PotionOrder> comparator;
