c63b73c46fce0ec98f41ae052d700aa206908365
Frank Hansen <frankhvidbjerg@gmail.com>
2024-10-30 17:24:15 +0100

    Move potion comparator to its own class

--- src/main/java/work/fking/masteringmixology/PotionOrderSorting.java
+++ src/main/java/work/fking/masteringmixology/PotionOrderSorting.java
@@ -4,22 +4,7 @@ import java.util.Comparator;
 
 public enum PotionOrderSorting {
     VANILLA("Vanilla (random)", null),
-    // Sort by modifier, in the order CRYSTALISED > HOMOGENOUS > CONCENTRATED
-    BY_STATION("By station", Comparator
-            .comparingInt((PotionOrder order) -> {
-                switch (order.potionModifier()) {
-                    case CRYSTALISED:
-                        return 0;
-                    case HOMOGENOUS:
-                        return 1;
-                    case CONCENTRATED:
-                        return 2;
-                    default:
-                        return Integer.MAX_VALUE;
-                }
-            })
-            .thenComparing(order -> order.potionType().name()) // Secondary sorting by PotionType name alphabetically
-    );
+    BY_STATION("By station", PotionComparators.byStation());
 
     private final String name;
     private final Comparator<PotionOrder> comparator;
