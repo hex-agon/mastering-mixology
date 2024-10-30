eb462f3134b7e2d7e680b44df915e15f9ca479fd
Colton <ColtonJamesCampbell@gmail.com>
2024-10-30 19:25:51 -0300

    Added shortest path potion sorting option (#85)

--- src/main/java/work/fking/masteringmixology/PotionComparators.java
+++ src/main/java/work/fking/masteringmixology/PotionComparators.java
@@ -3,7 +3,6 @@ package work.fking.masteringmixology;
 import java.util.Comparator;
 
 public class PotionComparators {
-
     // Sort by modifier, in the order CRYSTALISED > HOMOGENOUS > CONCENTRATED
     // And then by PotionType name alphabetically
     public static Comparator<PotionOrder> byStation() {
@@ -20,5 +19,20 @@ public class PotionComparators {
                     }
                 })
                 .thenComparing(order -> order.potionType().name());
+    }
+
+    public static Comparator<PotionOrder> shortestPath() {
+        return Comparator.comparing(order -> {
+            switch (order.potionModifier()) {
+                case CRYSTALISED:
+                    return 1;
+                case CONCENTRATED:
+                    return 2;
+                case HOMOGENOUS:
+                    return 3;
+                default:
+                    throw new IllegalStateException("Unexpected value: " + order.potionModifier().toString());
+            }
+        });
     }
 }
